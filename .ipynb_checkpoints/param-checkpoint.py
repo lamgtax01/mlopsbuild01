3110771a8a132ec27b193ce5b5c8e7d34b6085dc
Lambert <dtlamberto@gmail.com>
2024-06-19 18:56:52 +0000

    02

---
+++ .ipynb_checkpoints/param-checkpoint.py
@@ -0,0 +1,4 @@
+processing_instance_type =  "ml.m5.xlarge"
+processing_instance_count = 1
+training_instance_type =  "ml.m5.xlarge"
+training_instance_count = 1
---
+++ param.py
@@ -0,0 +1,7 @@
+# Processing Instance 
+processing_instance_type =  "ml.m5.xlarge"
+processing_instance_count = 1
+
+# Training Instance 
+training_instance_type =  "ml.m5.xlarge"
+training_instance_count = 1
